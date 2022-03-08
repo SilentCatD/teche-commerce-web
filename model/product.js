c07b344cd3ed05b46041fa9196cddfd8e87b9c33
ZabitTank <bestmatch236@gmail.com>
2022-03-08 18:32:08 +0700

    Initiate Product model

---
+++ model/product.js
@@ -0,0 +1,34 @@
+// TODO: Review this
+
+// import mongoose from "mongoose";
+//
+// const productScheme = new mongoose.Schema({
+//     name:{
+//       type: String,
+//       required: true
+//     },
+//     category: [String],
+//     publisher: String,
+//     rewviewCount: {
+//       type: Number,
+//       min: 0,
+//       default: 0,
+//     },
+//     reviewRate: {
+//       type: Number,
+//       min: 0,
+//       max:5,
+//       default: 0
+//     },
+//     quantity: {
+//       type: Number,
+//       min: 0,
+//       default: 0
+//     } ,
+//     price: Number,
+//     imageObjectId: mongoose.SchemaTypes.ObjectId
+// });
+//
+// const Brand = mongoose.model('Brand',brandSchema);
+//
+// export default Brand;
