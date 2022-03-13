777e567e17b0a6ff7b55ab2800e9ef66ef2ba249
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-03-14 02:17:37 +0700

    add field rateCount to product

--- model/product.js
+++ model/product.js
@@ -16,6 +16,11 @@ const productSchema = new mongoose.Schema({
         default: 0,
         min: 0
     },
+    rateCount: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
     images: [mongoose.SchemaTypes.ObjectId],
 
     details: String,
