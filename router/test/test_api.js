30b640d1e56b6cb462299c4371893de7bbd5254b
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-03-25 10:42:00 +0700

    add validator

--- router/test/test_api.js
+++ router/test/test_api.js
@@ -1,42 +1,55 @@
-import {Router} from 'express';
-import {} from 'faker';
+import { Router } from "express";
 const testApiRouter = Router();
-import {faker} from '@faker-js/faker';
-import Category from '../../components/category/model.js';
-import isInt from '../../utils/is_int.js';
-testApiRouter.get('/', async (req, res) => {
-    res.render('test');
+import { faker } from "@faker-js/faker";
+import Category from "../../components/category/model.js";
+import { query, validationResult } from "express-validator";
+testApiRouter.get("/", async (req, res) => {
+  res.render("test");
 });
 
-testApiRouter.get('/gen-cat', async (req, res)=>{
-    for(let i = 0 ; i < 100; i++){
-        const cat =  new Category({name: faker.name.findName()});
-        await cat.save();
+testApiRouter.get("/gen-cat", async (req, res) => {
+  for (let i = 0; i < 100; i++) {
+    const cat = new Category({ name: faker.name.findName() });
+    await cat.save();
+  }
+  res.status(200).send("gen completed");
+});
+
+testApiRouter.get(
+  "/get-cat",
+  query("page")
+    .if(query("page").exists())
+    .isInt({ min: 1 })
+    .withMessage("page must be interger in range 1-...")
+    .toInt(),
+  query("limit")
+    .if(query("limit").exists())
+    .isInt({ min: 1 })
+    .withMessage("limit  must be interger in range 1-...")
+    .toInt(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.status(200).send('gen completed');
-});
-
-
-testApiRouter.get('/get-cat/', async (req, res)=>{
-    const {page, limit} = req.query;
+    const { page, limit } = req.query;
     // check isInt by express-validator => important
     // missing page or limit is fine
-    if (page<1){
-        return res.status(404).send('invalid page request');
-    }
+
     const totalCount = await Category.countDocuments();
     const totalPages = Math.ceil(totalCount / limit);
-    const categories = await Category.find().skip((limit * page) - limit).limit(limit);
+    const categories = await Category.find()
+      .skip(limit * page - limit)
+      .limit(limit);
     const result = {
-        ...(limit && {'total-pages': totalPages}),
-        ...(limit && {'current-page': page ? parseInt(page): 1}),
-        'total-item': totalCount,
-        'item-count': categories.length,
-        'items': categories,
+      ...(limit && { "total-pages": totalPages }),
+      ...(limit && { "current-page": page ? parseInt(page) : 1 }),
+      "total-item": totalCount,
+      "item-count": categories.length,
+      items: categories,
     };
     res.status(200).json(result);
-});
-
-
+  }
+);
 
 export default testApiRouter;
