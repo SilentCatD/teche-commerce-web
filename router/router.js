c52ad239b3f7f9b3725868562defe560fbc0a441
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-03-31 01:27:57 +0700

    remove unndeed

--- router/router.js
+++ router/router.js
@@ -3,13 +3,11 @@ import apiRouter from "./api/api.js";
 import adminPageRouter from './frontend/admin_page.js';
 import webPageRouter from "./frontend/web_page.js";
 import testRouter from "./test/test.js";
-import testApiRouter from "./test/test_api.js";
 
 const router = express.Router();
 router.use('/api', apiRouter);
 router.use('/admin',adminPageRouter);
 router.use('/test', testRouter);
-router.use('/test-api', testApiRouter);
 router.use('/', webPageRouter);
 
 export default router;
