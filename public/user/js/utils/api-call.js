dd346ea47f7461b880721dc370d609121399e209
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2022-04-01 05:22:49 +0700

    Refactor, Add More API Call

--- public/user/js/utils/api-call.js
+++ public/user/js/utils/api-call.js
@@ -1,46 +1,62 @@
-function getHeaders() {
-  const headers = {};
-  const token = getJwtToken();
-  if (token) headers["Authorization"] = `Bearer ${token}`;
-  return headers;
-}
+
+import authentication from "./auth.js";
 
 const API_CALL = {
+  // tested
   loginRequest: async (email, password) => {
-    let res = await postEmailAndPassWord("login", email, password); // 200 | sucess)
+    let res = await usingForEveryFuckingRequest("post","login", email, password,null); // 200 | sucess)
     return res;
   },
-  registerRequestUser: async (email, password) => {
-    try {
-      const request = {
-        method: "post",
-        url: `/api/v1/auth/register`,
-        data: {
-          email: email,
-          password: password,
-        },
-      };
-      let res = await axios(request); // 200 | sucess
-      return res;
-    } catch (e) {
-      return e.response; // 400 validation failed
-    }
+  // tested
+  registerUserRequest: async (email, password) => { // tested
+    let res = await usingForEveryFuckingRequest("post","register", email, password,null); // 200 | sucess)
+    return res;
   },
+  // tested
+  newTokenRequest: async () => {
+    let res = await usingForEveryFuckingRequest("get","token", null, null,authentication.getRefreshToken()); // 200 | sucess)
+    return res;
+  },
+  // tested (i am lie)
+  logoutRequest: async () => {
+    let res = await usingForEveryFuckingRequest("get","logout", null, null,authentication.getRefreshToken()); // 200 | sucess)
+    return res;
+  },
+  // not tested
+  resendActiveAccountRequest: async (email) => {
+    let res = await usingForEveryFuckingRequest("post","resend-activate-email",email, null,null); // 200 | sucess)
+    return res;
+  },
+  // not test
+  registerAdminRequest: async (email, password) => { 
+    let res = await usingForEveryFuckingRequest("post","register-admin", email, password,authentication.getJwtToken()); // 200 | sucess)
+    return res;
+  }
 };
 
-async function postEmailAndPassWord(type, email, password) {
+
+async function usingForEveryFuckingRequest(type,route, email, password,token) {
   try {
     const request = {
-      method: "post",
-      url: `/api/v1/auth/${type}`,
-      data: {
-        email: email,
-        password: password,
-      },
+      method: `${type}`,
+      url: `/api/v1/auth/${route}`,
+      data: {},
     };
+    if(email) {
+      request.data.email = email;
+    }
+    if(password) {
+      request.data.password = password;
+    }
+    if(token) {
+      request.headers = authentication.getHeaders(token);
+    }
+    console.log(request);
     const res = await axios(request);
     return res;
   } catch (e) {
     return e.response; // 400 validation failed | 403 wrong password
   }
 }
+
+export default API_CALL;
--- public/user/js/utils/auth.js
+++ public/user/js/utils/auth.js
@@ -1,18 +1,29 @@
 // Short duration JWT token (5-10 min)
-const authentication = {
+ const authentication = {
     getJwtToken: () => {
-        return sessionStorage.getItem("jwt")
+        return localStorage.getItem("jwt")
     },
     getRefreshToken: () => {
-        return sessionStorage.getItem("refreshToken")
+        return localStorage.getItem("refreshToken")
     },
     setRefreshToken: (token) =>{
-        sessionStorage.setItem("refreshToken", token)
+        localStorage.setItem("refreshToken", token)
     },
     setJwtToken: (token) => {
-        sessionStorage.setItem("jwt", token)
+        localStorage.setItem("jwt", token)
+    },
+    getHeaders: (token) => {
+        const headers = {};
+        if (token) headers["Authorization"] = `Bearer ${token}`;
+        return headers;
+    },
+    removeToken: () => {
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("jwt");
     },
     sayHello: () => {
         console.log("hello");
-    }
-}
+    },
+};
+
+export default authentication;
