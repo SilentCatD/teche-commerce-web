e45942338a75470b1cd3493a713fdc45032f99b3
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-04-05 17:14:21 +0700

    add func to api service

--- public/utils/api_service.js
+++ public/utils/api_service.js
@@ -94,7 +94,177 @@ const APIService = {
   haveTokens: async(role) => {
     const token = TokenService.refreshToken.get(role);
     if(token) return true;
-  }
+  },
+  createProduct: async ({
+    productName,
+    productDetails,
+    productPrice,
+    productUnit,
+    productBrand,
+    productCategory,
+    productsImages,
+  }) => {
+    let formData = new FormData();
+    formData.append("productName", productName);
+    formData.append("productDetails", productDetails);
+    formData.append("productPrice", productPrice);
+    formData.append("productUnit", productUnit);
+    formData.append("productBrand", productBrand);
+    formData.append("productCategory", productCategory);
+    productsImages.forEach((file) => {
+      formData.append("images", file);
+    });
+    const url = "/api/v1/product";
+    const body = formData;
+    const headers = { "Content-Type": "multipart/form-data" };
+    const res = await Request.post({
+      url: url,
+      body: body,
+      headers: headers,
+      role: "admin",
+      useToken: true,
+    });
+  },
+
+  fetchAllProduct: async ({
+    page,
+    limit,
+    sort = "createdAt",
+    order_by = "desc",
+    range_field,
+    min,
+    max,
+  } = {}) => {
+    const url = "/api/v1/product";
+    const searchParams = queryAllParamsFormat(
+      page,
+      limit,
+      sort,
+      order_by,
+      range_field,
+      min,
+      max
+    );
+    const res = await Request.get({ url: url, params: searchParams });
+    return res.data.data;
+  },
+  fetchAllBrand: async ({
+    page,
+    limit,
+    sort = "createdAt",
+    order_by = "desc",
+    range_field,
+    min,
+    max,
+  } = {}) => {
+    const url = "/api/v1/brand";
+    const searchParams = queryAllParamsFormat(
+      page,
+      limit,
+      sort,
+      order_by,
+      range_field,
+      min,
+      max
+    );
+    const res = await Request.get({ url: url, params: searchParams });
+    return res.data.data;
+  },
+  fetchAllCategory: async ({
+    page,
+    limit,
+    sort = "createdAt",
+    order_by = "desc",
+    range_field,
+    min,
+    max,
+  } = {}) => {
+    const url = "/api/v1/category";
+    const searchParams = queryAllParamsFormat(
+      page,
+      limit,
+      sort,
+      order_by,
+      range_field,
+      min,
+      max
+    );
+    const res = await Request.get({ url: url, params: searchParams });
+    return res.data.data;
+  },
+
+  deleteProduct: async (id) => {
+    const url = `/api/v1/product/${id}`;
+    await Request.delete({ url, role: "admin", useToken: true });
+  },
+
+  editProduct: async ({
+    id,
+    productName,
+    productDetails,
+    productPrice,
+    productUnit,
+    productBrand,
+    productCategory,
+    productsImages,
+  }) => {
+    let formData = new FormData();
+    formData.append("productName", productName);
+    formData.append("productDetails", productDetails);
+    formData.append("productPrice", productPrice);
+    formData.append("productUnit", productUnit);
+    formData.append("productBrand", productBrand);
+    formData.append("productCategory", productCategory);
+    for (let i = 0; i < productsImages.length; i++) {
+      if (productsImages[i] instanceof File) {
+      } else {
+        productsImages[i] = await urlToFile(productsImages[i]);
+      }
+      formData.append("images", productsImages[i]);
+    }
+    const url = `/api/v1/product/${id}`;
+    const body = formData;
+    const headers = { "Content-Type": "multipart/form-data" };
+    await Request.put({ url, body, headers, role: "admin", useToken: true });
+  },
+
+  fetchProduct: async (id) => {
+    const url = `/api/v1/product/${id}`;
+    const res = await Request.get({ url });
+    return res.data.data;
+  },
+
+  createBrand: async ({ brandName, imgFile }) => {
+    let formData = new FormData();
+    if (imgFile) {
+      formData.append("images", imgFile);
+    }
+    formData.append("brandName", brandName);
+    const url = "/api/v1/brand";
+    const body = formData;
+    const headers = { "Content-Type": "multipart/form-data" };
+    await Request.post({
+      url: url,
+      body,
+      headers,
+      useToken: true,
+      role: "admin",
+    });
+  },
+  deleteBrand: async (id) => {
+    const url = `/api/v1/brand/${id}`;
+    await Request.delete({ url, role: "admin", useToken: true });
+  },
+  createCategory: async ({ categoryName }) => {
+    const body = { categoryName };
+    const url = "/api/v1/category";
+    await Request.post({ url, body, role: "admin", useToken: true });
+  },
+
+  deleteCategory: async (id) => {
+    const url = `/api/v1/category/${id}`;
+    await Request.delete({ url, role: "admin", useToken: true });
+  },
 };
 
 export default APIService;
